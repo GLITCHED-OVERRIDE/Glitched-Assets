b4e724e7a8c853d3472dc300aa1296058cb91c3e
1-Agent-Glitch-1 <Agent.Glitch.GLITCHOVERRIDE@gmail.com>
2025-11-07 14:53:45 -0600

    Add files via upload

---
+++ Proxys/UV/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/$/',
+    bare: 'https://petezahserver.faizinternational.com.np/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
